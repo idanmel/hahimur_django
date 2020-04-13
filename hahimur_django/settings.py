d4b526d35663c200603415f1796928fd0bc87713
idanmel <idanmel@gmail.com>
2020-04-13 19:31:32 +0300

    Migrated the Tournament model

--- hahimur_django/settings.py
+++ hahimur_django/settings.py
@@ -31,13 +31,13 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'tournaments.apps.TournamentsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'tournaments',
 ]
 
 MIDDLEWARE = [
