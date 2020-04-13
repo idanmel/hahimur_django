fd5dd2bdfbc9859d5bf2f25916c82d32144f55b6
idanmel <idanmel@gmail.com>
2020-04-13 20:23:43 +0300

    prettier register list

--- tournaments/admin.py
+++ tournaments/admin.py
@@ -2,4 +2,10 @@ from django.contrib import admin
 
 from .models import Tournament, GroupMatch, KnockOutMatch, GroupMatchPrediction, KnockOutMatchPrediction
 
-admin.site.register([Tournament, GroupMatch, KnockOutMatch, GroupMatchPrediction, KnockOutMatchPrediction])
+admin.site.register([
+    Tournament,
+    GroupMatch,
+    KnockOutMatch,
+    GroupMatchPrediction,
+    KnockOutMatchPrediction,
+])
