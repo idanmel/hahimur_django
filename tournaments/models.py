b8c02e8a74c69ff2bd97aed906fb05d41fb7c847
idanmel <idanmel@gmail.com>
2020-04-15 17:54:20 +0300

    added unique contraint to GroupMatchPrediction

--- tournaments/models.py
+++ tournaments/models.py
@@ -1,5 +1,6 @@
 from django.conf import settings
 from django.db import models
+from django.db.models import UniqueConstraint
 
 
 class Tournament(models.Model):
@@ -48,6 +49,11 @@ class GroupMatchPrediction(models.Model):
     def __str__(self):
         return f"{self.friend}, {self.tournament} {self.match_number}. " \
                f"{self.home_score} - {self.away_score}"
+
+    class Meta:
+        constraints = [
+            UniqueConstraint(fields=['tournament', 'friend', 'match_number'], name='unique_group_match_prediction')
+        ]
 
 
 class KnockOutMatchPrediction(models.Model):
