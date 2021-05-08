ddcd6c5ddcc6090bbd5e5cbb0850f4e7b5b4799d
Idan Melamed <idanmel@gmail.com>
2021-05-08 10:39:35 +0300

    updaed cors orogin whitelist

--- hahimur_django/settings.py
+++ hahimur_django/settings.py
@@ -127,8 +127,8 @@ STATIC_URL = '/static/'
 
 CORS_ORIGIN_WHITELIST = [
     "https://hahimur.com",
-    "http://localhost:8001",
-    "http://127.0.0.1:8001"
+    "http://localhost:8000",
+    "http://127.0.0.1:8000"
 ]
 
 DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
